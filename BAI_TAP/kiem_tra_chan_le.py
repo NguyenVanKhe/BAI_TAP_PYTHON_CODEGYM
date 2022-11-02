3512d27d18f8173b1c9e61a31245bc741c19af0c
NguyenVanKhe <kag.automation97@gmail.com>
2022-11-02 21:06:31 +0700

    Kiem tra so chan le

---
+++ BAI_TAP/kiem_tra_chan_le.py
@@ -0,0 +1,19 @@
+'''
+
+nhap_so = int(input("nhập một số bắt kì:"))
+hien_thi= "SÔ chẵn " if nhap_so % 2==0 else "Số lẻ"
+print (hien_thi)
+
+'''
+
+nhap_so=int(input("Nhập một số bất kì:"))
+
+if nhap_so % 2 ==0:
+    print ("Số chẵn")
+else:
+    print ("Số lẻ")
+ 
+
+
+
+             
