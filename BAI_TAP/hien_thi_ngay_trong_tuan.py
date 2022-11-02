9859cd67eee9681a7c4e9c765b74ea48ce928a66
NguyenVanKhe <kag.automation97@gmail.com>
2022-11-02 21:32:00 +0700

    hien_thi_ngay_trong_tuan.py

---
+++ BAI_TAP/hien_thi_ngay_trong_tuan.py
@@ -0,0 +1,19 @@
+a =int(input(" Nhập số từ 1 đến 7:"))
+
+if a==1:
+    print ("thứ 2")
+elif a==2:
+    print ("thứ 3")
+elif a==3:
+    print ("thứ 4")
+elif a==4:
+    print ("thứ 5")
+elif a==5:
+    print ("thứ 6")
+elif a==6:
+    print ("thứ 7")
+elif a==7:
+    print ("Chủ nhật")
+    
+else:
+    print ("Nhập số trong khoảng 1-7")
