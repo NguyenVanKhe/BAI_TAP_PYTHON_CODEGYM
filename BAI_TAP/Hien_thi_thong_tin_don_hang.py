4405a3962558c2716a02b90b986e6fec46a022ec
NguyenVanKhe <kag.automation97@gmail.com>
2022-11-02 21:53:00 +0700

    Hien_thi_thong_tin_don_hang.py

---
+++ BAI_TAP/Hien_thi_thong_tin_don_hang.py
@@ -0,0 +1,14 @@
+a=float(input("Số tiền người dùng chi tại cửa hàng:"))
+
+if a>=150 :
+    print ("Tổng số tiền:" , a-50)
+   # print ("a-15")
+elif a>=100 :
+    print ("Tổng số tiền:" ,a-25)
+   # print ("a-25")
+elif a>=75:
+    print ("Tổng số tiền:" , a-15)
+   # print ("a-50")
+else:
+    print ("Tổng số tiền :" , a)
+        
