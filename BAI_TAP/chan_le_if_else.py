10fa88d08c961305ceb5f52d3f5cbdab2aacada9
NguyenVanKhe <kag.automation97@gmail.com>
2022-11-02 21:20:33 +0700

    Kiem tra chan le su dung if elif else

---
+++ BAI_TAP/chan_le_if_else.py
@@ -0,0 +1,11 @@
+a = float(input(" nhập số a bất kì:"))
+
+if a % 2 ==0 :
+    print ("Số chẵn")
+elif a % 2 ==1:
+    print ("Số lẻ")
+else:
+    print("Không phải số tự tự nhiên")
+          
+          
+          
