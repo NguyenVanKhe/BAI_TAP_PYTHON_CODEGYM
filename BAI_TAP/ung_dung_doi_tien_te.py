b2b9d46c9440f5b6da877bed7e5df6d37ebfaa75
NguyenVanKhe <kag.automation97@gmail.com>
2022-11-04 20:54:09 +0700

    Ung dung chuyen doi tien te

---
+++ BAI_TAP/ung_dung_doi_tien_te.py
@@ -0,0 +1,9 @@
+TI_GIA=float(input("Nhập tỉ giá:"))
+USD =float(input(" Nhập số tiền USD cần chuyển đổi: "))
+
+VND = USD*TI_GIA
+
+
+print("Số tiền USD sau khi chuyển đổi sang VNĐ với tỉ giá {} là:  {}".format(TI_GIA,VND))
+           
+             
